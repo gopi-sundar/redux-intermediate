e90eddf991e2f149331700dc29a9cee1d52200c4
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2018-01-15 15:29:47 -0600

    5 Ajax request(axios) and redux-promise middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,19 @@
-const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
+import axios from 'axios';
 
+const API_KEY = '90299d206d3556b0ed05ceedbe30a952';
+const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER= 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    
+    console.log('Request:', request);
+
+    return {
+        type:FETCH_WEATHER,
+        payload: request
+    };
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import WeatherReducer from '../reducers/reducer_weather';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  state: (state = {}) => state,
+  weatherReducer:WeatherReducer
 });
 
 export default rootReducer;
